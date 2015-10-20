14e105f17295676c0d87a3570f89144ec223d1e9
kmcdonald1494 <mcdonald.kayla94@gmail.com>
2015-10-20 14:19:28 -0400

    First edit, also created test

--- Reversal.java
+++ Reversal.java
@@ -1,15 +1,38 @@
 import java.io.*;
+import java.util.*;
 
 public class Reversal {
 	
 	public static void reverseFile(File input, File output){
-		try{
+		try {
 			FileReader read = new FileReader(input);
 			BufferedReader  bread = new BufferedReader(read);
+			PrintWriter writeOut = new PrintWriter(output, "UTF-8");
+			
+			ArrayList<String> words = new ArrayList<String>();
+			
+			String nextLine = bread.readLine();
+			while(nextLine != null) {
+				words.add(bread.readLine());
+			}
+			
+			for(int i=words.size()-1;i>=0;i--) {
+				String[] sentence = words.get(i).split(" ");
+				
+				for(int j=sentence.length-1;j>=0;j--) {
+					writeOut.print(sentence[j]);
+					writeOut.println();
+				}
+			}
+
+			writeOut.close();
 			
 		}
-		catch(FileNotFoundException e){
-			System.out.println(input + "does not exist");
+		catch(FileNotFoundException e) {
+			System.out.println(input + " does not exist");
+		}
+		catch(IOException e){
+			System.out.println(input + " is empty");
 		}
 	}
 

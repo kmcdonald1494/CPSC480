c66a6f327f9aaee969779d057e6ed741236d2a3a
kmcdonald1494 <mcdonald.kayla94@gmail.com>
2015-10-21 21:40:41 -0400

    corrected code, created files and expected output files

--- Reversal.java
+++ Reversal.java
@@ -2,15 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class Reversal {
-	
-	public static void main(String[] args){
-		
-		File inputFile = new File("/Volumes/KAYLER_DOCS/480/Reversal/src/cat.txt");
-		File outputFile = new File("/Volumes/KAYLER_DOCS/480/Reversal/src/catOut.txt");
-		reverseFile(inputFile, outputFile);
-		
-	}
-	
+
 	public static void reverseFile(File input, File output){
 		try {
 			FileReader read = new FileReader(input);
@@ -22,16 +14,15 @@ public class Reversal {
 			String nextLine;
 			while((nextLine = bread.readLine()) != null) {
 				words.add(nextLine);
-				System.out.println(bread.readLine());
 			}
 			
 			for(int i=words.size()-1;i>=0;i--) {
 				String[] sentence = words.get(i).split(" ");
 				
 				for(int j=sentence.length-1;j>=0;j--) {
-					writeOut.print(sentence[j]);
-					writeOut.println();
+					writeOut.print(sentence[j] + " ");
 				}
+				writeOut.println();
 			}
 
 			read.close();
